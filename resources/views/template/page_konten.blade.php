fe658dc2e81f4063519772b4c6c8eaedb6742a82
AfifahHasnaaaa <163675808+AfifahHasnaaaa@users.noreply.github.com>
2024-07-08 00:29:42 +0700

    Add files via upload

---
+++ resources/views/template/page_konten.blade.php
@@ -0,0 +1,14 @@
+<!-- Default box -->
+<div class="card">
+	<div class="card-header">
+		<h3 class="card-title">
+            @yield('judul')
+        </h3>
+	</div>
+	<div class="card-body">
+        @yield('konten')
+	</div>
+	<div class="card-footer">
+
+	</div>
+</div>
